7baa7963015214978dea2f09451064b440b6fa69
davicus <davicus@imperialrome.city>
2021-02-26 14:59:57 -0800

    Fix slide movement and store velocity after moving so that the character does not get stuck in corners. Also fix some warnings around aspect ratio and fonts

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,9 +4,9 @@ extends KinematicBody2D
 func _ready():
 	print("Hello World Player...")
 
-const MAX_SPEED = 150
-const ACCELERATION = 20
-const FRICTION = 75
+const MAX_SPEED = 80
+const ACCELERATION = 750
+const FRICTION = 750
 
 var velocity = Vector2.ZERO
 
@@ -17,10 +17,11 @@ func _physics_process(delta):
 	
 	if input_vector != Vector2.ZERO:
 		input_vector = input_vector.normalized()
-		velocity += input_vector * ACCELERATION * delta
-		velocity = velocity.clamped(MAX_SPEED * delta)
+		velocity = velocity.move_toward(input_vector * MAX_SPEED,  ACCELERATION * delta)
+		#velocity += input_vector * ACCELERATION * delta
+		#velocity = velocity.clamped(MAX_SPEED * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	
-	move_and_collide(velocity)
+	#Save the velocity for the next frame to prevent sticking in corners
+	velocity = move_and_slide(velocity)
