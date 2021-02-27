8c727bc8d60a97fc49480e1e2fddd533f4c7b203
davicus <davicus@imperialrome.city>
2021-02-26 16:05:23 -0800

    Added AnimationTree and blend states to make player animate in the travel direction. P6

--- Player/Player.gd
+++ Player/Player.gd
@@ -10,6 +10,11 @@ const FRICTION = 750
 
 var velocity = Vector2.ZERO
 
+onready var animation_player = $AnimationPlayer
+onready var animation_tree = $AnimationTree
+onready var animation_state = animation_tree.get("parameters/playback")
+
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -18,9 +23,11 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		input_vector = input_vector.normalized()
 		velocity = velocity.move_toward(input_vector * MAX_SPEED,  ACCELERATION * delta)
-		#velocity += input_vector * ACCELERATION * delta
-		#velocity = velocity.clamped(MAX_SPEED * delta)
+		animation_tree.set("parameters/Idle/blend_position", input_vector)
+		animation_tree.set("parameters/Run/blend_position", input_vector)
+		animation_state.travel("Run")
 	else:
+		animation_state.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	#Save the velocity for the next frame to prevent sticking in corners
