e987b5272e6aa59c5954c4a3fd028c1599beb10b
davicus <davicus@imperialrome.city>
2021-03-12 14:37:09 -0800

    Added grass background, dirt and cliff tilemaps, attack animation frames, and initial refactor to introduce move-attack-roll state machine. Parts 6-9

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,12 +2,19 @@ extends KinematicBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("Hello World Player...")
+	animation_tree.active = true
 
 const MAX_SPEED = 80
 const ACCELERATION = 750
 const FRICTION = 750
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
 var velocity = Vector2.ZERO
 
 onready var animation_player = $AnimationPlayer
@@ -16,6 +23,9 @@ onready var animation_state = animation_tree.get("parameters/playback")
 
 
 func _physics_process(delta):
+	move_state(delta)
+	
+func move_state(delta):	
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
