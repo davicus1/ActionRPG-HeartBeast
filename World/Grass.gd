1aeb2fdfa62d30e68a7f6bbf3643a54fd0e26eab
davicus <davicus@imperialrome.city>
2021-03-19 15:33:59 -0700

    Hitboxes and hurtboxes, and sword disabled until attacking with disabled frames and masks PT11

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
+func create_grass_effect():
 		var GrassEffect = load("res://Effects/GrassEffect.tscn")
 		var grass_effect = GrassEffect.instance()
 		var world = get_tree().current_scene
 		grass_effect.global_position = global_position
 		world.add_child(grass_effect)
-		
-		queue_free()
+
+
+
+func _on_Hurtbox_area_entered(area):
+	create_grass_effect()
+	queue_free()
