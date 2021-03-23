2f717b6f072405025ec7638da2aae91b75a78b84
davicus <davicus@imperialrome.city>
2021-03-23 11:44:38 -0700

    Added damage and no_health signal so Bat enemy can be defeated PT14

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -2,8 +2,10 @@ extends KinematicBody2D
 
 var knockback = Vector2.ZERO
 
+onready var stats = $Stats
+
 func _ready():
-	pass
+	print("%s/%s" % [stats.health,stats.max_health])
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200*delta)
@@ -12,4 +14,11 @@ func _physics_process(delta):
 
 func _on_Hurtbox_area_entered(area):
 	knockback = area.knockback_vector * 130
-	#queue_free()
+	print("Bat: OW!!!")
+	stats.health -= area.damage
+	
+
+
+func _on_Stats_no_health():
+	print("Bat: I have been defeated, farewell!")
+	queue_free()
